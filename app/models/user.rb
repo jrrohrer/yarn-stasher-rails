27672e2e4d471a5d627833535721ef1b17195c9e
jrrohrer <jessie.rohrer@gmail.com>
2021-05-18 17:12:47 -0400

    add User resource and associations

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :yarns
+  has_many :projects
+  has_many :comments
+  has_many :commented_projects, through: :comments, source: :projects
+  # has_many :projects, through: :comments
+end
